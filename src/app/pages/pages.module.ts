2b264443a1729ec11fb4594b665aca5297ee0d9c
Cuza <cuzahechavarria@gmail.com>
2021-12-13 19:37:54 +0100

    creando lo basico del sitio

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+
+import { ShareModule } from './share/share.module';
+
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [CommonModule, ShareModule],
+  exports: [ShareModule, LoginComponent],
+})
+export class PagesModule {}
